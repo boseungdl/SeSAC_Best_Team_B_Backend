75682160b5dd968c3402b904f8b7356ebb2caaf4
jaewoong <kimje1109@gmail.com>
2023-09-05 17:02:32 +0900

    feature: 로그인한 유저의 글과 이미지 조회 기능 추가

--- src/controllers/getdataController.ts
+++ src/controllers/getdataController.ts
@@ -28,3 +28,31 @@ export const getUserById = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+export const getRecordsWithImages = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user;
+    const records = await Record.findAll({
+      where: { kakaoId: userId },
+      attributes: ["recordId", "recordValue"]
+    });
+
+    if (!records) {
+      return res.status(404).json({ error: 'No records found' });
+    }
+
+    const recordsWithImages = [];
+    for (const record of records) {
+      const images = await Image.findAll({
+        where: { recordId: record.recordId },
+        attributes: ["id", "GPSLatitude", "GPSLongitude", "CreateDate"],
+      });
+
+      recordsWithImages.push({ record, images });
+    }
+
+    return res.json(recordsWithImages);
+  } catch (error) {
+    res.status(500).send('서버 오류가 발생했습니다.');
+  }
+};
